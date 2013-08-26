73e44a82c4e891fc13a5e542e61d95e5124ec202
Diadara <nithin111@gmail.com>
2013-08-26 23:40:42 +0530

    changed module name to indicdictionary and started version no from .1.0

--- setup.py
+++ setup.py
@@ -2,17 +2,16 @@
 
 from setuptools import setup, find_packages
 
-name = "dictionary"
+name = "indicdictionary"
 
 setup(
     name = name,
-    version = "0.2.1",
-    url = "http://silpa.org.in/dictionary",
+    version = "0.1.0",
     license = "LGPL-3.0",
     description = "A bilingual dictionary for indic languages",
     author = "Santhosh Thottingal",
     author_email = "santhosh.thottingal@gmail.com",
-    long_description = """A bilingual dictionary supporting multiple indic languages and english""",
+    long_description = """A bilingual dictionary supporting multiple indic languages and English""",
     packages = find_packages(),
     include_package_data = True,
     setup_requires = ['setuptools-git'],
